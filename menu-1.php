d601dc5a5c75dc6760f52eddb60df75b84ea94ad
rejena123 <36025270+rejena123@users.noreply.github.com>
2018-04-16 17:57:35 +0800

    Update menu-1.php

--- menu-1.php
+++ menu-1.php
@@ -18,11 +18,11 @@ body{
 <p><a href="ivy.php" target="content">Ivy Villaignacio</a>
 <p><a href="ang.php" target="content">Angel Ann Flores</a>
 <p><a href="ar.php" target="content">Arlene T. Alfeche</a>
-<p><ahref="jeff.php" target="content">Jeff Alicante</a>
-<p><ahref="but.php" target="content">Anton Clifford Villarta</a>
-<p><ahref="inang.php" target="content">Rejena Grace S. Pitogo</a>
+<p><a href="jeff.php" target="content">Jeff Alicante</a>
+<p><a href="but.php" target="content">Anton Clifford Villarta</a>
+<p><a href="inang.php" target="content">Rejena Grace S. Pitogo</a>
 <p><a href="tol.php" target="content">Cristylle A. Tayco</a>
-<p><ahref="drey.php" target="content">Andrey Jade Alferez</a>
+<p><a href="drey.php" target="content">Andrey Jade Alferez</a>
 <p><a href="rich.php" target="content">Richard Jim N. Aliganga</a>
 <p><a href="mel.php" target="content">Carmel A. Tayco</a></p>
 
